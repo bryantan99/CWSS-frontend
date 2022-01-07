0848cf35d58d7560f07b3ed6c71aef3a7f7fce80
BRYAN TAN <tanjiaqin99@gmail.com>
2022-01-08 01:12:42 +0800

    - replaced localhost with environment.apiUrl

--- src/app/user/signup.service.ts
+++ src/app/user/signup.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignupService {
 
-  readonly apiServerUrl = 'http://localhost:8080';
+  readonly apiServerUrl = environment.apiUrl;
 
   readonly REGISTER_ACCOUNT = this.apiServerUrl + "/register";
 
